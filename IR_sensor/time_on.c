3e26d9a2232f6a374530a633e14cc1a95687a8b9
Epsilon391 <ebarnes500@icloud.com>
2024-04-14 13:07:47 -0400

    Signed-off-by: Epsilon391 <ebarnes500@icloud.com>

--- IR_sensor/time_on.c
+++ IR_sensor/time_on.c
@@ -8,48 +8,53 @@
 #define IR 4 
 
 void init_timer();
-unsigned long millis();
+unsigned long millis_det();
 
 volatile unsigned long millis_counter = 0;
+volatile uint8_t object_detected = 0;
 
 int main(void)
 {
-	//Setup
-	LCD_Setup();
-	init_timer(); // Initialize the timer
-	LCD_Clear();
-	
-	while(1) 
-	{
-		if((PINB & (1 << IR)) == 0)
-		{
-			sei(); // Enable global interrupts
-			LCD_GotoXY(0, 0);
-			LCD_PrintString("Detected For:");
-			LCD_GotoXY(0, 1);
-			LCD_PrintInteger(millis());
-			LCD_PrintString(" ms");
-		} else {
-			cli(); // Disable global interrupts
-		}
-	}
+    // Setup
+    LCD_Setup();
+    init_timer(); // Initialize the timer
+    LCD_Clear();
+    
+    while(1) 
+    {
+        if((PINB & (1 << IR)) == 0)
+        {
+            object_detected = 1; // Set the flag
+            LCD_GotoXY(0, 0);
+            LCD_PrintString("Detected For:");
+            LCD_GotoXY(0, 1);
+            LCD_PrintInteger(millis_det());
+            LCD_PrintString(" ms");
+        } else {
+            object_detected = 0; // Clear the flag
+        }
+    }
 }
 
 ISR(TIMER1_COMPA_vect)
 {
-	millis_counter++;
+    if (object_detected == 1) {
+        millis_counter++;
+    }
 }
+
 
 void init_timer()
 {
-	// Configure Timer 1 for a 1ms interrupt
-	TCCR1B |= (1 << WGM12) | (1 << CS11) | (1 << CS10); // CTC mode, prescaler = 64
-	OCR1A = 250; // Timer compare value for 1ms at 16MHz
-	TIMSK1 |= (1 << OCIE1A); // Enable Timer 1 compare match interrupt
+    // Configure Timer 1 for a 1ms interrupt
+    TCCR1B |= (1 << WGM12) | (1 << CS11) | (1 << CS10); // CTC mode, prescaler = 64
+    OCR1A = 250; // Timer compare value for 1ms at 16MHz
+    TIMSK1 |= (1 << OCIE1A); // Enable Timer 1 compare match interrupt
+    sei(); // Enable inturrupts
 }
 
-unsigned long millis()
+unsigned long millis_det()
 {
-	return millis_counter;
+    return millis_counter;
 }
 
